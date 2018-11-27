b05cfe500ca48be12230ed75470ae5b9344ca9c6
kmunozhip <kmunoz@hipconsult.com>
2018-11-27 17:11:20 -0500

    Added health scatter plot

--- investor-script1.R
+++ investor-script1.R
@@ -3,12 +3,13 @@ install.packages("rJava")
 install.packages("openxlsx")
 
 #Load Required Packages
-library(xlsx)
+library(openxlsx)
 library(ggplot2)
-library(dplyr)
+library(plyr)
 
 #Import Data
 findexPage1 <- read.xlsx("findex-data/globalFindexdatabase.xlsx", sheet=1)
+providers <- read.csv('findex-data/provider_metrics.csv')
 str(findexPage1$X4)
 summary(findexPage1$X3)
 
@@ -37,19 +38,45 @@ p1 <- ggplot(asiaDonut, aes(fill=category, ymax=ymax, ymin=ymin, xmax=4, xmin=3)
 p1
 
 #Dummy Data Fiber Reach Bar Chart 
-asiaFRbar <- data.frame(value=c(40, 62, 34, 60, 45, 75, 38, 48), 
-  country=c('Cambodia', 'Indonesia', 'Lao PDR', 'Myanmar', 'Malaysia', 'Philippines', 'Thailand', 'Vietnam'))
+asiaFRbar <- data.frame(value=c(40, 62, 34, 60, 45, 75), 
+  country=c('Cambodia', 'Indonesia', 'Myanmar', 'Malaysia', 'Philippines', 'Thailand'))
 
 p2 <- ggplot(asiaFRbar, aes(country, value)) +
   geom_col( aes(fill=country)) +
-  theme_light() +
   scale_fill_brewer(palette = 'Set2') +
-  geom_hline(yintercept=72, color='#0D6EB8', size=1) +
+  geom_hline(yintercept=72, color='#0D6EB8', size=1.5) +
   coord_flip() +
   scale_y_continuous(limits = c(0,80), expand = c(0, 0))
 p2
 
-#Dummy Data Scatterplot
-asiaScatter <- data.frame(value=c(), fr=c())
+#Providers By Country Bar Chart
+asiaProviders <- subset(providers, continent == 'Asia')
+asiaProviders <- asiaProviders[c(3:6)]
+asiaProviders <- count(asiaProviders, "country")
+asiaProviders <- asiaProviders[asiaProviders$country %in%
+  c('Cambodia', 'Indonesia', 'Myanmar', 'Malaysia', 'Philippines', 'Thailand'),]
+asiaProviders
+
+p3 <- ggplot(asiaProviders, aes(country, freq)) +
+  geom_col(aes(fill=country)) +
+  scale_fill_brewer(palette = 'Set2') +
+  scale_y_continuous(limits = c(0,10), expand = c(0, 0)) +
+  coord_flip()
+p3
 
 #Dummy Data Line Chart
+asiaScatter <- read.csv("findex-data/final.csv", TRUE, sep =",")
+asiaScatter <- dplyr::rename(asiaScatter, country = ï..Country)
+asiaScatter <- dplyr::transmute(asiaScatter,
+  country=country,FB.Growth.Rate=FB.Growth.Rate*100,IMR=IMR*100,LE.Rate=LE.Rate*100)
+asiaScatter <- subset(asiaScatter, country %in% 
+  c('Cambodia', 'Indonesia', 'Myanmar', 'Malaysia', 'Philippines', 'Thailand'))
+
+p4 <- ggplot(asiaScatter, aes(x=FB.Growth.Rate, y=IMR, color=LE.Rate)) +
+  geom_point(size=5)+
+  geom_smooth(se = 0, color="black", linetype="solid", method=lm) +
+  scale_color_gradient(low='#65C7F7', high='#0052D4') +
+  ggrepel::geom_label_repel(data=asiaScatter, 
+    aes(label = asiaScatter$country), direction='both', color = 'black', nudge_x = 1.5)
+p4
+  
